e45508c094ad44a2f2198d46e6761cf61954f39d
unknown <shemregidor07@gmail.com>
2025-08-27 21:36:58 +0800

    Add Notification Table

--- kcera-be/database/migrations/2025_08_26_145724_create_notifications_table.php
+++ kcera-be/database/migrations/2025_08_26_145724_create_notifications_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,15 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('receiver_id')->nullable()->constrained('users')->onDelete('cascade');
+            $table->string('receiver_type')->nullable();
+            $table->foreignId('report_id')->nullable()->constrained('emergency_reports')->onDelete('cascade');
+            $table->foreignId('response_id')->nullable()->constrained('emergency_responses')->onDelete('cascade');
+            $table->string('type')->nullable();
+            $table->string('title')->nullable();
+            $table->text('message')->nullable();
+            $table->boolean('is_read')->default(false);
+            $table->timestamp('read_at')->nullable();
             $table->timestamps();
         });
     }
